62c46cbe804fd9a797aedc48a309ecf0b1af9aa3
arielbergamini <arielbergamini263@gmail.com>
2026-04-02 14:04:45 -0700

    hardcoded beary friendly info into quest ui

---
+++ Assets/Scripts/Dialogue/DialogueController.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour
+{
+    public static DialogueController Instance { get; private set; }
+
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    public Image portraitImage;
+
+    public NPCDialogue dialogueData;
+    private DialogueController dialogueUI;
+    public int ID;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void ShowDialogueUI(bool show)
+    {
+        dialoguePanel.SetActive(show);
+    }
+
+    public void SetNPCInfo (string npcName, Sprite portrait)
+    {
+        nameText.text = npcName;
+        portraitImage.sprite = portrait;
+    }
+
+    public void SetDialogueText(string text)
+    {
+        dialogueText.text = text;
+    }
+}
